bec01ffc5419c2b57f48a0d29cfe5fa821f88619
Yug Bhatia <bhatiayug175@gmail.com>
2026-04-04 20:01:43 +0530

    test

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-app.options("*", cors());
 app.use(express.json());
 app.use(bodyParser.json({
   strict: true, // Ensures only valid JSON is parsed
